a403891e8143eafc7e5fb4a35c120cffd6590e70
nwilson1412 <nathanrwilson@hotmail.com>
2017-11-05 22:25:06 +0930

    Web3 addition and testing

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,6 +7,35 @@ import { HomeComponent } from './home/home.component';
 import { BalanceComponent } from './balance/balance.component';
 import { WalletComponent } from './wallet/wallet.component';
 
+
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from "@angular/http";
+import { Observable } from "rxjs";
+//import Web3 from 'web3';
+
+
+const Web3 = require('web3');
+//var web3 = new Web3(new Web3.providers.HttpProvider("http://58.7.40.25:8546"));
+//this.web3.isConnected();
+
+
+
+//let web3:Web3 = new Web3();
+var web3 = new Web3(new Web3.providers.HttpProvider("http://58.7.40.25:8546"));
+console.log("terawtwta")
+
+var ethbalance = web3.eth.getBalance('0x000000000000000000000000000000000000dead');
+
+console.log(ethbalance);
+
+var number = web3.eth.getBlockNumber;
+
+console.log(number);
+
+
+
+
+
 @NgModule({
     declarations: [
         HomeComponent,
