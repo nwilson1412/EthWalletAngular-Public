9fb970a6c9e4cb25e8b1c18bd8940169ada06fd2
nwilson1412 <nathanrwilson@hotmail.com>
2017-11-05 20:48:22 +0930

    Add web3 call in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import Web3 from 'web3';
+var web3 = new Web3();
+window.console.log(web3);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
