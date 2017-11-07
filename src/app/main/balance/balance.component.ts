11dddd5dadc9196db33a63b07e8c2f50ff365f09
nwilson1412 <nathanrwilson@hotmail.com>
2017-11-07 15:24:38 +0930

    Clean up homme and add account bal web3 call
    Removed redundant code from /home. Added hardcoded address and address and balance call.

--- src/app/main/balance/balance.component.ts
+++ src/app/main/balance/balance.component.ts
@@ -13,6 +13,10 @@ export class BalanceComponent {
     successData: any;
     successBlockNumber: number;
     currentBlock: number;
+    webProvider: any;
+    netGeth: any ;
+    coinbase: any;
+    coinBal: any;
 
     gethBlock = {
         difficulty: '',
@@ -25,22 +29,41 @@ export class BalanceComponent {
 
     ngOnInit():void {
 
-    //this.web3 = getweb3();
-    this.web3 = new Web3(new Web3.providers.HttpProvider("http://58.7.40.25:8546"))
-    //this.web3 = new web3p.providers.HttpProvider("http://58.7.40.25:8546");
-
+    this.web3 = getweb3();
+    //this.webProvider = new Web3.providers.HttpProvider("http://58.7.40.25:8546");
+    //this.web3 = new Web3(this.webProvider)
+    //this.web3 = new web3.providers.HttpProvider("rcd");
+   //
    // this.web3.eth.gethBlock
 
-    this.web3.eth.getBlockNumber().then((successBlockNumber) => {
-        this.currentBlock = successBlockNumber;
-    //  console.log(this.currentBlock);
-    });
+   console.log("isConnected=");
+   this.web3.eth.net.isListening().then((netlistenGeth) =>{
+        this.netGeth = netlistenGeth;
+        console.log(this.netGeth);
+   });
 
-    this.web3.eth.getBlock(0).then((successData) => {
-        this.gethBlock = successData;
-        console.log(this.gethBlock);
-        console.log(this.currentBlock);
-    });
+   this.coinbase = '0x000000000000000000000000000000000000dead';
+   //this.web3.fromWei(this.web3.eth.getBalance(account),"ether");
+   console.log('this is the account; ' + this.coinbase)
+
+   this.web3.eth.getBalance(this.coinbase).then((coin) =>{
+       coin = coin / 1000000000000000000;
+        this.coinBal = coin
+
+        console.log(coin)
+   });
+
+
+   this.web3.eth.getBlockNumber().then((successBlockNumber) => {
+    this.currentBlock = successBlockNumber;
+//  console.log(this.currentBlock);
+});
+
+this.web3.eth.getBlock(0).then((successData) => {
+    this.gethBlock = successData;
+    console.log(this.gethBlock);
+    console.log(this.currentBlock);
+});
 
 }}
 
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import getweb3 from '../../components/web3';
-import web3p from 'web3';
+
+//import web3p from 'web3';
 
 
 
@@ -13,24 +13,12 @@ import web3p from 'web3';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-    x: number;
-    web3: any;
+export class HomeComponent implements OnInit {    
     //web3a: any;
     cryptoList: any;
     ethStats: any;
     //gethBlock: any;
-    successData: any;
-    accountx: any;
-    gethBlock = {
-        difficulty: '',
-        gasLimit: '', 
-        };
-    
 
-    successData2: number;
-    currentBlock: number;
-    aa: any;
 
     constructor(private http: HttpClient){
 
@@ -46,40 +34,6 @@ export class HomeComponent implements OnInit {
         this.http.get('https://api.coinmarketcap.com/v1/ticker/?convert=AUD').subscribe(data => {
         this.cryptoList = data;
          });
-        
-        
-        
-         this.web3 = getweb3();
-         //this.web3 = new web3p.providers.HttpProvider("http://58.7.40.25:8546");
-
-
-        this.web3.eth.getBlockNumber().then((successData2) => {
-            this.currentBlock = successData2;
-         //   console.log(this.currentBlock);
-        });
-
-        this.web3.eth.getBlock(0).then((successData) => {
-              this.gethBlock = successData;
-              console.log(this.gethBlock);
-              console.log(this.currentBlock);
-        });
-
-        /*
-        this.web3.eth.personal.getAccounts.then((successData) => {
-            this.gethBlock = successData;
-            console.log(this.gethBlock);
-            console.log(this.currentBlock);
-      })*/
-
-        // /return this.gethBlock;
-
-
-        
-        
-       // var bknum = this.web3.eth.getBlockNumber;
-        //console.log(bknum) //this shows a function, not the number(?)
-        
-        
         
     }
 
