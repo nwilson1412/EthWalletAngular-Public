a0fc661962b8a21a234a2a50aa99623244f632c7
Isaac <isaac01011100@gmail.com>
2017-11-07 22:03:07 +0930

    Extracted and refactored web3 functions

--- src/app/main/balance/balance.component.ts
+++ src/app/main/balance/balance.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import getweb3 from '../../components/web3';
 import Web3 from 'web3';
-
-
 
 @Component({
     selector: 'balance-view',
     templateUrl: './balance.component.html',
     styleUrls: ['./balance.component.css']
- 
 })  
-
-
 
 export class BalanceComponent implements OnInit {
     web3: any;
+
+    // ISaac vars
+    addressInput: string;
+    userAddress: string;
+    userBalance: number;
+    userTransactions: number;
+    userBlockNumber: number;
+    userBlockData: any;
+    currentBLockNumber: number;
+    currentBlockData: any;
+
+
+    //Nath vars
     successData: any;
     successBlockNumber: number;
     currentBlock: number;
@@ -25,64 +34,82 @@ export class BalanceComponent implements OnInit {
     gethBlock: any;
     userIn: any;
 
-
     constructor(){
-
-
+        this.setupWeb3();
     }
     
+    ngOnInit():void {}
 
-
-
-    gethAccountInput(UIn:string) {
-          this.userIn = UIn;
-        
+    /* Component calls (from the HTML view) */
+    loadAddressData(userAddress){
+        this.userAddress = userAddress;
+        this.getBalance(userAddress).then((response) => {
+            this.userBalance = response / 1000000000000000000;
+            // this.getTransactions(userAddress).then((response) => {
+            //     this.userTransactions = response;
+            // });
+        });
     }
-    onClickMe(){
-        this.gethAccountInput(this.userIn);
-        alert("this is a test" + " hello " +this.userIn);
+
+    /* Web 3 calls */
+    getStatus(){
+        return this.web3.eth.net.isListening();
     }
-        
 
-    ngOnInit():void {
+    getBalance(address: string){
+        return this.web3.eth.getBalance(address);
+    }
 
-    this.web3 = getweb3();
+    // This was in the API but apparently doesn't work? 
+    // getTransactions(address: string){
+    //     return this.web3.eth.getTransaction(address);
+    // }
 
-    //this.web3 = new Web3(new Web3.providers.HttpProvider("http://58.7.40.25:8546"));
-    //this.web3 = new Web3(this.webProvider)
-    //this.web3 = new web3.providers.HttpProvider("rcd");
-   //
-   // this.web3.eth.gethBlock
+    getCurrentBlockNumber(){
+        return this.web3.eth.getBlockNumber();
+    }
 
-   
-   this.web3.eth.net.isListening().then((netlistenGeth) =>{
-        this.netGeth = netlistenGeth;
-        console.log("isConnected=" + this.netGeth);
-   });
+    getCurrentBlock(){
+        this.getCurrentBlockNumber().then((response) => {
+            return this.getBlockData(response);
+        });
+    }
 
-   this.coinbase = '0x000000000000000000000000000000000000dead';
-   //this.web3.fromWei(this.web3.eth.getBalance(account),"ether");
-   console.log('this is the account; ' + this.coinbase)
+    getBlockData(blockNumber: number){
+        return this.web3.eth.getBlock(blockNumber);
+    }
 
-   this.web3.eth.getBalance(this.coinbase).then((coin) =>{
-       coin = coin / 1000000000000000000;
-        this.coinBal = coin
-
-        console.log(coin)
-   });
+    /* SETUP STUFF */
+    setupWeb3(): void{
+        this.web3 = getweb3();
+    }
 
 
-   this.web3.eth.getBlockNumber().then((successBlockNumber) => {
-    this.currentBlock = successBlockNumber;
-//  console.log(this.currentBlock);
-});
+    /* Just left these in for reference if you wanted theM? they're all part of the functions above now though. 
+    delete if you don't need */
 
-this.web3.eth.getBlock(0).then((successData) => {
-    this.gethBlock = successData;
-    console.log(this.gethBlock);
-    console.log(this.currentBlock);
-});
+    // this.coinbase = '0x000000000000000000000000000000000000dead';
+    // //this.web3.fromWei(this.web3.eth.getBalance(account),"ether");
+    // console.log('this is the account; ' + this.coinbase)
 
-}}
+    // this.web3.eth.getBalance(this.coinbase).then((coin) =>{
+    //     coin = coin / 1000000000000000000;
+    //     this.coinBal = coin
+
+    // });
+
+
+    // this.web3.eth.getBlockNumber().then((successBlockNumber) => {
+    //     this.currentBlock = successBlockNumber;
+    // //  console.log(this.currentBlock);
+    // });
+    
+    // this.web3.eth.getBlock(0).then((successData) => {
+    //     this.gethBlock = successData;
+    //     console.log(this.gethBlock);
+    //     console.log(this.currentBlock);
+    // });
+
+}
 
 
